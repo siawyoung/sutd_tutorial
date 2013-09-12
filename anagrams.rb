6f5f4c4fd82531cdf67cf2c1b0a385dac3f3a93f
Siaw Young <lausiawyoung@gmail.com>
2013-09-12 13:28:04 +0800

    Added anagrams.rb

---
+++ anagrams.rb
@@ -0,0 +1,13 @@
+def anagrams (word1, word2)
+  if(word1.split("").sort.join==word2.split("").join)
+	puts "Win"
+  else
+	puts "Lose"
+  end
+
+end
+
+test = gets
+test1 = gets
+
+anagrams(test,test1)
